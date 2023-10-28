aa986aacc556984b9dfcfaf4390a1af5deb45324
Billy272 <billylaroi@gmail.com>
2023-10-28 09:32:42 +0300

    commit

--- R_projects/dataframes.R
+++ R_projects/dataframes.R
@@ -1,9 +1,10 @@
 library(dplyr)
 
 names <- c("John", "Billy", "Sanjay", "Sally", "Jane")
-age <- c(18,17,20,19,21)
+date_born <- as.Date(c("2005-04-20", "2002-11-18", "2002-10-15", "2004-09-12", "2002-08-10"))
+age <- c(18,20,21,19,21)
 height <- c(180,187,194,172,175)
 
-df <- data.frame(names, age, height)
+df <- data.frame(names,date_born, age, height)
 
 print(df)
