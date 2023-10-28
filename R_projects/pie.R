1169a31dffb780607e6a66353fe26f24a1ebbc78
Billy272 <billylaroi@gmail.com>
2023-10-28 09:47:28 +0300

    commit

--- R_projects/pie.R
+++ R_projects/pie.R
@@ -1,8 +1,10 @@
+library(plotrix)
+
 x <- c(21,62,10,53)
 labels <- c("London", "New York", "Singapore", "Mumbai")
 
-png(file = "city.png")
+png(file = "3d_pie.png", width = 600, height = 600)
 
-pie(x, labels)
+pie3D(x, labels = lbl, explode = 0.1, main = "3D Pie Chart for Cities")
 
 dev.off()
