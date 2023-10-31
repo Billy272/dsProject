b35e08348751357ae5998b523f0b24957a7ec121
Billy272 <billylaroi@gmail.com>
2023-10-31 08:42:50 +0300

    commit

--- R_projects/Titanic.R
+++ R_projects/Titanic.R
@@ -1,2 +1,7 @@
+library(ggplot2)
 
+data <- read.csv("train.csv")
+
+ggplot(data, aes(x = Age, y = Fare)) + geom_boxplot(fill = "lightblue", color = "blue") + labs(title = "Boxplot of Age vs Fare", x = "Age", y = "Fare")+
+  theme_minimal()
 
